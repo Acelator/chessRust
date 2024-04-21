2bc31599a69d19ab793dba4605640243d0c2ac2e
Pablo Cordon Blanco <corbla04@gmail.com>
2024-04-22 00:05:14 +0200

    create basic board struct

--- chess/src/board/mod.rs
+++ chess/src/board/mod.rs
@@ -1,5 +1,59 @@
-pub fn t() {
-
-    print!("Jello hello")
+#[derive(Copy, Clone)]
+pub enum Color {
+    White,
+    Black
 }
 
+#[derive(Copy, Clone)]
+pub enum Pieces {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King
+}
+
+
+pub struct Board {
+    pieces: [u64; 6] ,
+    player_pieces: [u64; 2],
+    
+    player_to_move: Color,
+    castling_rights: [bool; 2],
+    halfmove_clock: u8,
+
+}
+
+impl Board {
+    // TODO: Take FEN notation to create a board at a set point. STARTING POS => rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+    // TODO: Change to hexadecimal values
+    pub fn new(s: String) -> Board {
+        if s.is_empty() {
+           Board{pieces: [71776119061282560, 4755801206503243842, 2594073385365405732, 9295429630892703873, 576460752303423504, 1152921504606846984], 
+                 player_pieces: [65535, 18446462598732840960], player_to_move: Color::White, castling_rights: [true, true], halfmove_clock: 0} 
+        } else {
+            // DO stuff
+            Board{pieces: [71776119061282560, 4755801206503243842, 2594073385365405732, 9295429630892703873, 576460752303423504, 1152921504606846984], 
+               player_pieces: [65535, 18446462598732840960], player_to_move: Color::White, castling_rights: [true, true], halfmove_clock: 0}
+        }
+    } 
+
+    pub fn get_board(&mut self) -> u64 {
+       self.player_pieces[0] | self.player_pieces[1]
+    }
+
+    pub fn get_pieces(&mut self, piece: &Pieces, player: Option<&Color>) -> u64 {
+        match player {
+            Some(c) => self.player_pieces[*c as usize] & self.pieces[*piece as usize],
+            None => self.pieces[*piece as usize]
+        }
+    }
+
+    pub fn get_player_pieces(&mut self, player: &Color, pieces: Option<&Pieces>) -> u64 {
+        match pieces {
+            Some(p) => self.player_pieces[*player as usize] & self.pieces[*p as usize],
+            None => self.player_pieces[*player as usize]
+        }
+    }
+}
--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -1,6 +1,17 @@
+use std::string;
+
 use chess;
+use chess::board::*;
 
 #[test]
-fn main() {
-    assert_eq!(4, chess::m());
+fn initial_board() {
+    let s = String::new();
+
+    let mut board = Board::new(s);
+    assert_eq!(0xffff00000000ffff, board.get_board());
+    assert_eq!(0xff00000000ff00, board.get_pieces(&Pieces::Pawn, None));
+    assert_eq!(0x8, board.get_pieces(&Pieces::King, Some(&Color::White)));
+
+    assert_eq!(0xffff000000000000, board.get_player_pieces(&Color::Black, None));
+    assert_eq!(0x81, board.get_player_pieces(&Color::White, Some(&Pieces::Rook)));
 }
